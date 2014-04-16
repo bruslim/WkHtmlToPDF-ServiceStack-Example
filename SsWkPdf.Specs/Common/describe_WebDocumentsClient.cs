cadd7019f45b7ea2452c714479bfbf5024f0704e
Brian Ruslim <bpr0422@gmail.com>
2014-04-16 17:22:02 -0400

    added dummy spec for json client

---
+++ SsWkPdf.Specs/Common/describe_WebDocumentsClient.cs
@@ -0,0 +1,24 @@
+﻿using NSpec;
+using ServiceStack;
+using ServiceStack.MsgPack;
+using SsWkPdf.ServiceModel;
+
+namespace SsWkPdf.Specs.Common
+{
+    public class describe_WebDocumentsClient :nspec
+    {
+        void describe_Create()
+        {
+            it["should just work"] = () =>
+            {
+                IServiceClient client = new JsonServiceClient("http://localhost:14872");
+                var result = client.Post(
+                    new WebDocuments.CreateRequest
+                    {
+                        SourceUrl = "google.com"
+                    });
+                result.should_not_be_null();
+            };
+        }
+    }
+}
